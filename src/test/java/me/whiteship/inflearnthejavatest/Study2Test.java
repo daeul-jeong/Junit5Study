e2b73e97d80f4a15b445c492628e318acbc6c0b0
정다을(Elin) <elin.jeong@goodoc.co.kr>
2020-10-19 23:04:40 +0900

    JUnit 5 테스트 반복하기 2부

--- src/test/java/me/whiteship/inflearnthejavatest/Study2Test.java
+++ src/test/java/me/whiteship/inflearnthejavatest/Study2Test.java
@@ -2,9 +2,18 @@ package me.whiteship.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
@@ -93,8 +102,67 @@ class Study2Test {
 	@DisplayName("스터디 만들기")
 	@ParameterizedTest(name = "{index} {displayName} message={0}")
 	@ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+	@EmptySource //empty 케이스 하나 더 추가
+	@NullSource
+	@NullAndEmptySource
 	void parameterizedTest(String message) {
 		System.out.println(message);
+	}
+
+
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@ValueSource(ints = {10, 20, 40})
+	void parameterizedTest(Integer limit) {
+		System.out.println(limit);
+	}
+
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, 스프링"})
+	void parameterizedTest1(Integer limit, String name) {
+		Study study = new Study(limit, name);
+		System.out.println(study.getLimit());
+	}
+
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@ValueSource(ints = {10, 20, 40})
+	void parameterizedTest2(@ConvertWith(StudyConverter.class) Study study) { //암묵적으로 타입을 변환해줌.
+		System.out.println(study.getLimit());
+	}
+
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, 스프링"})
+	void parameterizedTest3(ArgumentsAccessor argumentsAccessor) {
+		Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+		System.out.println(study.getLimit());
+	}
+
+	static class StudyConverter extends SimpleArgumentConverter {//이건 1개의 argument만
+
+		@Override
+		protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+			assertEquals(Study.class, targetType, "can only covert to study");
+			return new Study(Integer.parseInt(source.toString()));
+
+		}
+	}
+
+	@DisplayName("스터디 만들기")
+	@ParameterizedTest(name = "{index} {displayName} message={0}")
+	@CsvSource({"10, '자바 스터디'", "20, 스프링"})
+	void parameterizedTest3(@AggregateWith(StudyAggregator.class) Study study){
+		System.out.println(study.getLimit());
+	}
+
+	static class StudyAggregator implements ArgumentsAggregator {
+		//static inner class나 public 클래스여야함.
+		@Override
+		public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+			return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+		}
 	}
 
 	//테스트 실행되기전에 딱 한번 실행됨
